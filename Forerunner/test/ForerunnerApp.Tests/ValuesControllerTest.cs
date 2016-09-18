6be87b4ee5223d9c5c5b27adbca32ef9d3f1e665
Danguolė Gaižiūtė <pelkiuzveris@gmail.com>
2016-09-18 22:46:00 +0300

    test project

---
+++ Forerunner/test/ForerunnerApp.Tests/ValuesControllerTest.cs
@@ -0,0 +1,22 @@
+﻿using ForerunnerApp.Controllers;
+using Xunit;
+
+namespace ForerunnerApp.Tests
+{
+    public class ValuesControllerTest
+    {
+        private readonly ValuesController _controller;
+
+        public ValuesControllerTest()
+        {
+            _controller = new ValuesController();
+        }
+
+        [Fact]
+        public void Get_ShouldReturnSomething()
+        {
+            var result = _controller.Get();
+            Assert.NotEmpty(result);
+        }
+    }
+}
